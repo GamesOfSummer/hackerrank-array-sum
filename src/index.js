3fbf5427189e6a239ebc1f1a960785b9a69709b3
Summer T <summer@summertime-development.com>
2021-08-09 13:36:10 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+var sockMerchant = function (numberOfSocks, array) {
+    var pair = 0;
+    array.sort();
+    for (var i = 1; i < numberOfSocks; i++) {
+        if (array[i - 1] === array[i]) {
+            pair++;
+            i++;
+        }
+    }
+    return pair;
+};
+var result = sockMerchant(3, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 
 
-console.log('hello world');
+
+
+
+
+let sockMerchant = (numberOfSocks : number, array : any) => {
+    
+
+let pair = 0;
+    array.sort();
+
+    for(let i = 1; i < numberOfSocks; i++)
+    {
+       if(array[i - 1] === array[i])
+        {
+            pair++;
+            i++;
+        }
+    }
+
+    return pair;
+}
+
+
+const result = sockMerchant(3, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+console.log(result);
